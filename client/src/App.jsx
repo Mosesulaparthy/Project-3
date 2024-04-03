e857cca81631993f3d37bd1f213110ae3fe9e543
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-04-03 19:47:42 -0400

    Added Signup page and utils(API and Auth) to client, added routes, user-controller to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/login";
+import SignupForm from "./pages/Signup";
 import RecipeSearch from "./pages/RecipeSearch";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   return (
     <div className="App">
       <Router>
@@ -16,9 +18,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupForm />} />
           <Route path="/search" element={<RecipeSearch />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/footer.css"
+const Footer = () => {
+  return (
+    <div className="footer">
+    <p>Created by: Arya Saini, Andrew Casablanca, Christian Douglas, Moses Mulaparthy</p>
+    </div>
+  );
+};
+
+export default Footer;
