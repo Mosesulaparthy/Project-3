b1144d6023e2733dd980008f0bf735d29ed347ab
Arya Saini <arya30012@gmail.com>
2024-04-04 19:06:43 -0400

    added user to resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,19 @@
 // const { gql } = require('apollo-server-express');
 
 const typeDefs = `
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
 type Query{
     getRecipeSuggestions(ingredients: [String]!): String
 }
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
 `;
 module.exports = typeDefs
