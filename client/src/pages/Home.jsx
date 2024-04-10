b4b4045047bd9ad41e7f202bcc3e81b0f1f60003
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-04-10 14:05:59 -0400

    Cleaned up pages with better looking CSS

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Auth from "../utils/auth"
+import "../styles/home.css"
 
 const Home = () => {
+  if (Auth.loggedIn()) {
   return (
-    <div>
+    <div className='welcome'>
+      <h1>Welcome to our website!</h1>
+      <h1> Access our <a href="/recipe-search">Recipe Finder</a> here or by clicking the search in the Navbar</h1>
+      <p><i>*This website is in early development so not all features are available</i></p>
     </div>
   );
+} else {
+  return (
+<div className='login'>
+      <h1> Please <a href="/login">login</a> in order to access our recipe search engine.</h1>
+    </div>
+  )
 };
+}
 
 export default Home;
